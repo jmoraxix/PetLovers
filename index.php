65c07ccaf63cf4e816ba25b40f2272b38d730309
Jose Mora <jdevmora@gmail.com>
2015-03-18 15:42:02 -0600

    Cambios en el diseño base.

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-	<title>Magnetic - Stunning Responsive HTML5/CSS3 Photography Wensite Template</title>
+	<title>PetLovers</title>
 	<meta charset="utf-8">
-	<meta name="author" content="pixelhint.com">
-	<meta name="description" content="Magnetic is a stunning responsive HTML5/CSS3 photography/portfolio website template"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
@@ -15,7 +13,7 @@
 
 	<header>
 		<div class="logo">
-			<a href="index.html"><img src="img/logo.png" title="Magnetic" alt="Magnetic"/></a>
+			<a href="index.html"><img src="img/logo.png" title="PetLovers" alt="PetLovers"/></a>
 		</div><!-- end logo -->
 
 		<div id="menu_icon"></div>
@@ -33,15 +31,10 @@
 			<ul class="social clearfix">
 				<li><a href="#" class="fb" data-title="Facebook"></a></li>
 				<li><a href="#" class="google" data-title="Google +"></a></li>
-				<li><a href="#" class="behance" data-title="Behance"></a></li>
-				<!--<li><a href="#" class="twitter" data-title="Twitter"></a></li>
-				<li><a href="#" class="dribble" data-title="Dribble"></a></li>-->
-				<li><a href="#" class="rss" data-title="RSS"></a></li>
 			</ul><!-- end social -->
 
 			<div class="rights">
-				<p>Copyright © 2014 magnetic.</p>
-				<p>Template by <a href="">Pixelhint.com</a></p>
+				<p>Copyright © 2015 PetLovers.</p>
 			</div><!-- end rights -->
 		</div ><!-- end footer -->
 	</header><!-- end header -->
