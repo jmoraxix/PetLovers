766918567266643b7841a22cb5f5aa8223f3f2c6
Adrián Díaz Azofeifa <adriandiazocr@gmail.com>
2015-03-31 14:23:19 -0600

    cambio pequeño en script 3

--- SQL scripts/3.CreateUser.sql
+++ SQL scripts/3.CreateUser.sql
@@ -6,7 +6,7 @@ por Adrian Diaz Azofeifa
 CREATE TABLE Usuario
 (
        username VARCHAR2(12) CONSTRAINT username_nn NOT NULL, /* El nombre de usuario*/
-       password VARCHAR2(16),  /*El password*/
+       user_password VARCHAR2(16),  /*El password*/
        user_type NUMBER(1) /*El tipo de usuario: administrador(1) o normal(0)*/
        
 );
