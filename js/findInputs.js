f1e4e2ad5f104812b36cb7a118dc6978ddeb7c78
Elang89 <elang8899@gmail.com>
2015-03-30 20:38:38 -0600

    PHP inserts, SQL scripts, cambios a findInputs

--- js/findInputs.js
+++ js/findInputs.js
@@ -1,5 +1,16 @@
+/* Made By Ernesto Lang 3/22/15, modified 3/30/15 */
+
+/* The following script retrieves the data from the html form found in createUser.html, first it creates an xmlHTTP object in order to send 
+ * data through ajax, once this is done an array is extracted from the html form, the relevant data, meaning the spaces that are not empty, is then 
+ * put into another array which is then placed into a JSON and sent to the server(PHP). Once the server responds, depending on whether it succeeded
+ * on passing the data to the database or not, a message will be displayed to the user through an alert in the browser
+ */
+
+
 window.onload = function findSubmitButton() {
-    var button = document.querySelector(".send_info").addEventListener("click", serverInteraction);
+    var button = document.querySelector(".send_info").addEventListener("click", serverInteraction); /*Unobtrusive javascript listener, added to 
+    																								create account button in html*/
+
 }
 
 function serverInteraction() {
@@ -9,16 +20,27 @@ function serverInteraction() {
   var JSONArray;
   var userId;
   if (window.XMLHttpRequest){
-	  xmlhttp = new XMLHttpRequest();
+	  xmlhttp = new XMLHttpRequest();        /* Used for IE7+,FireFox, Opera, Chrome, Safari */
   } else if (window.ActiveXObject) {
-	  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");   /* Compatibility for IE6 browsers */
   } else {
-	  throw new Error("Your Browser is not compatible with XMLHTTP");
+	  throw new Error("Your browser is not compatible with XMLHTTP");
 	  return false;
   }
+  
+  /* The following section validates if the required inputs have text in them and if the password fields match*/
   inputArray = document.querySelectorAll("input[type=text]")
+  if (inputArray[1].value != inputArray[2].value){
+	  alert("Password doesn't match, please make sure your password matches")
+	  return false;
+  }
+  
   for(var i = 0; i < inputArray.length; i++){
-	  finalArray[i] = inputArray[i].value;
+	  if (inputArray[i].value == ""){
+		  alert("Please fill out all of the fields");
+		  return false;
+	  }
+		finalArray[i] = inputArray[i].value;
   }
 	console.log(finalArray);
     JSONArray = JSON.stringify({finalArray: finalArray}); 
@@ -26,5 +48,5 @@ function serverInteraction() {
 	xmlhttp.open("POST","phpFiles/sendUserInfo.php", true);
 	xmlhttp.setRequestHeader("Content-type","application/json;charset=UTF-8");
 	xmlhttp.send(JSONArray);
-	console.log("status:" + xmlhttp.status);
+	console.log(xmlhttp.responseText);
 }
--- phpFiles/sendUserInfo.php
+++ phpFiles/sendUserInfo.php
@@ -1,12 +1,44 @@
 <?php
-	$connection = oci_connect('PetLoversScheme', 'pet', 'localhost/petloversdbXDB');
-	echo file_get_contents("php://input");
-	if(!$connection){
-		$e = oci_error();
-		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+	/* Made by Ernesto Lang 3/22/2015 */
+	/* The following code is used to receive data from the client-side(JavaScript/HTML), a JSON is sent through JavaScript by using AJAX and 
+	 * sending an XMLHTTP request. PHP retrieves the JSON data, decodes into a PHP array equivalent, which is then deconstructed, to insert data
+	 * into the appropriate database tables, once this is finished a response is sent back to the client-side application, which should indicate
+	 * whether PHP was succesful inserting the data into the database.
+	 */	
+
+	$db_connection = oci_connect('PetLoversScheme', 'pet', 'localhost/petloversdbXDB');  /*Used to establish connection between database and server*/																										  
+	$finalArray = file_get_contents('php://input');									  /*Data retrieval*/			
+	$finalArray = json_decode($finalArray,true); 					/*JSON decoding*/
+	echo var_dump($finalArray);
+	
+	$username = $finalArray['finalArray'][0];
+	$password = $finalArray['finalArray'][1];
+	$email = $finalArray['finalArray'][3];
+	$name = $finalArray['finalArray'][4];
+	$firstLastName = $finalArray['finalArray'][5];
+	$secondLastName = $finalArray['finalArray'][6];
+	
+	$sqlVariableAccountsTable = 'INSERT INTO accounts(username, user_password) VALUES(:username, :password)';  /* SQL statement to send */
+	$dataToInsert = oci_parse($db_connection, $sqlVariableAccountsTable);                         /* prepares statement to be sent to database */
+	
+	
+	                                     
+	if(!$db_connection){                                    /* checks if connection with the database works */
+		echo "Server could not connect to database";
 	}
 	else {
 		echo "Connection Established";
 	}
+	
+	oci_bind_by_name($dataToInsert, ':username', $username);                    /* binds variables to their string names */
+	oci_bind_by_name($dataToInsert, ':password', $password);
+	
+	
+	if(oci_execute($dataToInsert)){               /* statement is sent to the database, a print is made to indicate */
+		echo "User created succesfully";
+	} else {
+		echo "User could not be created";
+	}
+	oci_close($db_connection);
 	
 ?>
