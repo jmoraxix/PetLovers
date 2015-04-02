b6b034dd5ff027b9024daca0eb17b9c55c932798
José David Mora Loría <jdevmora@gmail.com>
2015-04-02 03:49:03 -0600

    Creacion javascripts de menu y manejo de de eventos-

---
+++ js/menu.js
@@ -0,0 +1,68 @@
+{
+  "menu": {
+    "publico": [
+      {
+        "h": "Home",
+        "a": "index",
+        "img": "fondo_blanco"
+      },
+      {
+        "h": "Find a pet",
+        "a": "index",
+        "img": "fondo_blanco"
+      },
+      {
+        "h": "View pets",
+        "a": "index",
+        "img": "fondo_blanco"
+      },
+      {
+        "h": "Top 10",
+        "a": "index",
+        "img": "fondo_blanco"
+      },
+      {
+        "h": "Chat",
+        "a": "index",
+        "img": "fondo_blanco"
+      },
+      {
+        "h": "Contact Us",
+        "a": "index",
+        "img": "contact"
+      }
+      ],
+    "admin": [
+      {
+        "h": "User administration",
+        "a": "inner",
+        "img": "administrar_usuarios"
+      },
+      {
+        "h": "Pet administration",
+        "a": "inner",
+        "img": "administrar_mascotas"
+      },
+      {
+        "h": "Category administration",
+        "a": "inner",
+        "img": "administrar_categorias"
+      },
+      {
+        "h": "View forms",
+        "a": "inner",
+        "img": "ver_formularios"
+      },
+      {
+        "h": "ver_lista_negra",
+        "a": "inner",
+        "img": "Black list"
+      },
+      {
+        "h": "View e-mails",
+        "a": "inner",
+        "img": "correo"
+      }
+  ]
+  }
+}
