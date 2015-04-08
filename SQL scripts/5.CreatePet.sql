a7f31f59dcc52225eb3b2f51185c33045e97c99f
Adrián Díaz Azofeifa <adriandiazocr@gmail.com>
2015-04-08 02:32:23 -0600

    Se actualizan scripts
    Traduccion al ingles, anotaciones

--- SQL scripts/5.CreatePet.sql
+++ SQL scripts/5.CreatePet.sql
@@ -1,11 +1,11 @@
-/*
-Script para crear la tabla de mascota
-por Adrian Diaz Azofeifa
+/* Made by Adrian Diaz Azofeifa on 03/31/2015
+
+Script for creating the Pet table
 */
 
 CREATE TABLE Pet
 (
        pet_name VARCHAR2(20) CONSTRAINT pet_name_nn NOT NULL, /* El nombre de la mascota*/
-	   pet_code NUMBER(10) CONSTRAINT pet_code_nn NOT NULL
+	   pet_code NUMBER(10)  /*PRIMARY KEY*/
        
 );
--- SQL scripts/9.AlterPerson.sql
+++ SQL scripts/9.AlterPerson.sql
@@ -1,7 +1,10 @@
-/*
-Se crea la llave primaria cedula con su indice
-por Adrian Diaz Azofeifa
+/* Made by Adrian Diaz Azofeifa on 03/31/2015
+Edited by Ernesto Lang Oreamuno on 04/04/2015
+
+Script for adding foreign key references and primary key constraints to Person table
+And to related tables Phone and Email
 */
+
 ALTER TABLE Person
   ADD CONSTRAINT pk_cedula PRIMARY KEY (person_id)
   USING INDEX
@@ -9,14 +12,14 @@ ALTER TABLE Person
   STORAGE (initial 10k next 10k pctincrease 0);
 
 /*
-Se crea la llave foranea para referenciar a usuario.
+Foreign key in Person Table for User Primary Key
 */
 
 ALTER TABLE Person
       ADD CONSTRAINT fk_user FOREIGN KEY (username) REFERENCES usuario(username);
 
 /*
-Modificamos las tablas de telefono y correo para referenciar a persona
+Foreign keys in Phone and Email tables for Person Primary Key
 */
 
 ALTER TABLE Phone
