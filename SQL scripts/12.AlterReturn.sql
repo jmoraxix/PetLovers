94a3de81c101d571c4876292cb38be292655d9cb
Adrián Díaz Azofeifa <adriandiazocr@gmail.com>
2015-04-10 17:33:37 -0600

    Se actualizan más scripts
    Correcciones menores.

--- SQL scripts/12.AlterReturn.sql
+++ SQL scripts/12.AlterReturn.sql
@@ -5,19 +5,7 @@ por Adrian Diaz Azofeifa
 */
 
 ALTER TABLE Return
-ADD CONSTRAINT pk_code PRIMARY KEY (return_code)
+ADD CONSTRAINT pk_return_code PRIMARY KEY (return_code)
 USING INDEX
 TABLESPACE PetLovers_Indexes PCTFREE 20
 STORAGE (initial 10k next 10k pctincrease 0);
-
-/*
-Se crea la llave foranea para referenciar a persona
-*/
-
-ADD CONSTRAINT fk_person FOREIGN KEY (person_id) REFERENCES person(cedula);
-
-/*
-Se crea la llave foranea para referenciar a mascota
-*/
-
-ADD CONSTRAINT fk_pet FOREIGN KEY (pet_id) REFERENCES pet(pet_code);
