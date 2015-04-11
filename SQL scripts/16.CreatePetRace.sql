30689d9a2a3ea08eedc90001a9d8c74ac586e8e4
Adrián Díaz Azofeifa <adriandiazocr@gmail.com>
2015-04-10 20:54:16 -0600

    nanocorrección a CreatePetRace u.u

--- SQL scripts/16.CreatePetRace.sql
+++ SQL scripts/16.CreatePetRace.sql
@@ -6,6 +6,6 @@ Script for creating Pet Race table
 CREATE TABLE PetRace
 (
 		pet_race_name VARCHAR(20) CONSTRAINT pet_race_name_nn NOT NULL, 
-		pet_race_code NUMBER(10) /*PRIMARY KEY*/
+		pet_race_code NUMBER(10), /*PRIMARY KEY*/
 		pet_type NUMBER(10) /*FOREIGN KEY REFERENCES PETTYPECODE*/
 );
