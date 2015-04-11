25b66287825fe68374316f98721464cc8b440b06
Adrián Díaz Azofeifa <adriandiazocr@gmail.com>
2015-04-10 21:58:10 -0600

    Se actualizan scripts de adopcion y devolucion

---
+++ SQL scripts/12.AlterPetReturn.sql
@@ -0,0 +1,11 @@
+/*
+Se crea la llave primaria codigo con su índice
+en la tabla de devolucion
+por Adrian Diaz Azofeifa
+*/
+
+ALTER TABLE PetReturn
+ADD CONSTRAINT pk_return_code PRIMARY KEY (return_code)
+USING INDEX
+TABLESPACE PetLovers_Indexes PCTFREE 20
+STORAGE (initial 10k next 10k pctincrease 0);
