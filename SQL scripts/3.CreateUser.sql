9e2a07fde1a4aadd50b10b063b021ea1d4d2afef
miumiu997 <miuyinyong@yahoo.com>
2015-04-11 20:14:37 -0600

    Devolver a original x error
    No funciono por choque con el paquete de usuario

--- SQL scripts/3.CreateUser.sql
+++ SQL scripts/3.CreateUser.sql
@@ -5,10 +5,10 @@ por Adrian Diaz Azofeifa
 
 CREATE TABLE Usuario
 (
-       user_name VARCHAR2(10), /* Users name*/ 
-       user_id NUMBER(10)  /* The primary key*/ 
-       user_password VARCHAR2(16),  /*password*/
-       user_type NUMBER(1) /*The type of user Administrative(1) or regular(0) */
+       username VARCHAR2(12), /* El nombre de usuario*/
+       user_password VARCHAR2(16),  /*El password*/
+       user_type NUMBER(1) /*El tipo de usuario: administrador(1) o normal(0)*/ 
+       
        
 );
 
