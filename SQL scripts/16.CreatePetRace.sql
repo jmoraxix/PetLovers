e0d0781d01cfa75cf717c01bd777f1a6e80053ee
miumiu997 <miuyinyong@yahoo.com>
2015-04-11 20:41:32 -0600

    Agrego el foreign key en petRace

--- SQL scripts/16.CreatePetRace.sql
+++ SQL scripts/16.CreatePetRace.sql
@@ -8,4 +8,7 @@ CREATE TABLE PetRace
 		pet_race_name VARCHAR(20) CONSTRAINT pet_race_name_nn NOT NULL, 
 		pet_race_code NUMBER(10), /*PRIMARY KEY*/
 		pet_type NUMBER(10) /*FOREIGN KEY REFERENCES PETTYPECODE*/
-);
+); 
+
+ALTER TABLE PetRace
+      ADD CONSTRAINT fk_petType FOREIGN KEY (pet_type) REFERENCES PetType(Pet_type_code);
