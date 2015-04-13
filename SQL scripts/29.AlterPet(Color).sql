b169bc38012e7bb3ecf23192e39fabfe8d717785
miumiu997 <miuyinyong@yahoo.com>
2015-04-12 18:14:09 -0600

    Elimino Color, createAdoption y createReturn
    color en la tabla pets se elimino porque no es necesario, create
    adoption y create return se eliminaron porque hay otras tablas llamadas
    PetAdoption y PetReturn que son mejore (por el nombre

--- SQL scripts/29.AlterPet(Color).sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE pet
-  ADD pet_color_code NUMBER(10);
-  
-
-ALTER TABLE pet
-      ADD CONSTRAINT fk_color_code FOREIGN KEY (pet_color_code) REFERENCES petColor(Pet_Color_Code); 
-      
-
