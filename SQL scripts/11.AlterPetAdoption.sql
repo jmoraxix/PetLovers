2557cf337dcc616a3623242f1b2847c4c5f14030
miumiu997 <miuyinyong@yahoo.com>
2015-04-12 19:45:21 -0600

    Se modifica que las tablas adoption
    se crean nuevos campos en la tabla para eliminar la relación. El código
    para modificar es:
    ALTER TABLE PETADOPTION
    ADD Giving_for_adoption NUMBER(10);
    
    ALTER TABLE PETADOPTION
    ADD ADOPTANT NUMBER(10);
    
    ALTER TABLE PETADOPTION
    ADD ACCEPTANCE_STATE NUMBER(1); /* ACEPTED (1) DENIAL(0)*/
    
    ALTER TABLE PETADOPTION
          ADD CONSTRAINT fk_giving_adopt_code FOREIGN KEY
    (Giving_for_adoption) REFERENCES Person(person_id);
    
    ALTER TABLE PETADOPTION
          ADD CONSTRAINT fk_adoptant_code FOREIGN KEY (ADOPTANT) REFERENCES
    Person(person_id);

--- SQL scripts/11.AlterPetAdoption.sql
+++ SQL scripts/11.AlterPetAdoption.sql
@@ -11,4 +11,10 @@ TABLESPACE PetLovers_Indexes PCTFREE 20
 STORAGE (initial 10k next 10k pctincrease 0);
 
 ALTER TABLE PetAdoption
-      ADD CONSTRAINT fk_Apet_code FOREIGN KEY (PET_ID) REFERENCES Pet(pet_code); 
+      ADD CONSTRAINT fk_Apet_code FOREIGN KEY (PET_ID) REFERENCES Pet(pet_code);  
+
+ALTER TABLE PETADOPTION
+      ADD CONSTRAINT fk_giving_adopt_code FOREIGN KEY (Giving_for_adoption) REFERENCES Person(person_id);
+  
+ALTER TABLE PETADOPTION
+      ADD CONSTRAINT fk_adoptant_code FOREIGN KEY (ADOPTANT) REFERENCES Person(person_id);
--- SQL scripts/6.CreatePetAdoption.sql
+++ SQL scripts/6.CreatePetAdoption.sql
@@ -7,6 +7,9 @@ CREATE TABLE PetAdoption
 (
        adoption_code VARCHAR2(20) CONSTRAINT adoption_code_nn NOT NULL, /* El codigo de la adopcion*/
        adoption_date DATE DEFAULT SYSDATE CONSTRAINT adoption_date_nn NOT NULL /* La fecha de la adopcion*/ 
-       pet_id NUMBER(10)
+       pet_id NUMBER(10) 
+	   Giving_for_adoption NUMBER(10); 
+	   ADOPTANT NUMBER(10); 
+	   ACCEPTANCE_STATE NUMBER(1); /* ACEPTED (1) DENIAL(0)*/
        
 );
