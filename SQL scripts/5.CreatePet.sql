d0c14202f845d85903f152d923632c302472f9b9
miumiu997 <miuyinyong@yahoo.com>
2015-04-18 14:45:29 -0600

    Agregar adopt_state
    Para saber si esta adoptado o no
    Para modificar sería:
    ADD ADOPT_STATE NUMBER(1); --  False(0) and True(1)

--- SQL scripts/5.CreatePet.sql
+++ SQL scripts/5.CreatePet.sql
@@ -6,6 +6,7 @@ Script for creating the Pet table
 CREATE TABLE Pet
 (
        pet_name VARCHAR2(20) CONSTRAINT pet_name_nn NOT NULL, /* El nombre de la mascota*/
-	   pet_code NUMBER(10)  /*PRIMARY KEY*/
+	   pet_code NUMBER(10)  /*PRIMARY KEY*/ 
+	   ADOPT_STATE NUMBER(1); --  False(0) and True(1) 
        
 );
