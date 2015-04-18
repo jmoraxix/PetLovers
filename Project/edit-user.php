6f845b532c5f60158820eef2591fdc6f4bef69ff
Santiago Méndez <santiago1096@gmail.com>
2015-04-18 03:03:09 -0600

    Cambios en botones de edit user

--- Project/edit-user.php
+++ Project/edit-user.php
@@ -19,16 +19,16 @@
         <h4>Change password</h4>
         <div class="row">
           <div class="col-lg-6">
-          <input id="user_old_password" type="password" class="form-control" placeholder="Old Password" name="form_phone" maxlength="16" value="">
+            <input id="user_old_password" type="password" class="form-control" placeholder="Old Password" name="form_phone" maxlength="16" value="">
           </div>
         </div>
         <div class="row">
           <div class="col-lg-6">
-          <input id="user_password" type="password" class="form-control" placeholder="New Password" name="form_phone" maxlength="16" value="">
+            <input id="user_password" type="password" class="form-control" placeholder="New Password" name="form_phone" maxlength="16" value="">
           </div>
           
           <div class="col-lg-6">
-          <input id="user_re_password" type="password" class="form-control" placeholder="Confirm Password" name="form_phone" maxlength="16" value="">
+            <input id="user_re_password" type="password" class="form-control" placeholder="Confirm Password" name="form_phone" maxlength="16" value="">
           </div>
         </div>
       </div>
@@ -44,13 +44,74 @@
         <label>Adopter</label> <br />
         <input type="checkbox" name="animal" value="" style="height: 20px">
         <label>Rescuer</label> <br />
+        <div class="row">
+          <div class="col-lg-6">
+            <button id="user_phones_butt" type="button" class="btn btn-success" data-toggle="modal" data-target="#user_phones">Edit Phones</button>
+          </div>
+          <div class="col-lg-6">
+            <button id="user_email_butt" type="button" class="btn btn-success" data-toggle="modal" data-target="#user_Email">Edit Email</button>
+          </div>
+        </div>
       </div>
     </div>
     <div class="row register">
       <div class="pull-right">
-        <button id="Submit_User" type="button" class="btn btn-success" name="Submit">Edit</button>
+        <button id="Submit_User" type="button" class="btn btn-success" name="Submit" >Save</button>
       </div>
     </div>
   </div>
 </div>
+<!-- Modal numbers-->
+<div id="user_phones" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="row">
+        <div class="col-sm-6">
+          <h4>Phone numbers of [USER]</h4>
+          <input id="user_phone" type="text" class="form-control" placeholder="Phone 1" name="form_name" maxlength="20" value="">
+          <input id="user_phone" type="text" class="form-control" placeholder="Phone 2" name="form_name" maxlength="20" value="">
+          <input id="user_phone" type="text" class="form-control" placeholder="Phone 3" name="form_name" maxlength="20" value="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="pull-right">
+          <div class="col-lg-6">
+            <button id="user_phones_cancel" type="button" class="btn btn-success">Cancel</button>
+          </div>
+          <div class="col-lg-6">
+            <button id="user_phones_save" type="button" class="btn btn-success">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /.modal numbers -->
+
+<!-- Modal emails -->
+<div id="user_Email" class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="row">
+        <div class="col-sm-6">
+          <h4>Emails of [USER]</h4>
+          <input id="user_email" type="text" class="form-control" placeholder="Email 1" name="form_name" maxlength="20" value="">
+          <input id="user_email" type="text" class="form-control" placeholder="Email 2" name="form_name" maxlength="20" value="">
+          <input id="user_email" type="text" class="form-control" placeholder="Email 3" name="form_name" maxlength="20" value="">
+        </div>
+      </div>
+      <div class="row">
+        <div class="pull-right">
+          <div class="col-lg-6">
+            <button id="user_Email_Cancel" type="button" class="btn btn-success">Cancel</button>
+          </div>
+          <div class="col-lg-6">
+            <button id="user_Email_Save" type="button" class="btn btn-success">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /.modal emails -->
 <?php include'footer.php';?>
