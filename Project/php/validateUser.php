588b2daaf05bbcacd15fbcf5b588467379c2ea91
José David Mora Loría <jdevmora@gmail.com>
2015-04-18 03:07:07 -0600

    Arreglo del logout y eliminacion de archivo innecesario

--- Project/php/validateUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['user_name']))
-{
-    session_start();
-    $_SESSION['user']=$_POST['user_name'];
-    //Storing the name of user in SESSION variable.
-} 
-header('location: ../index.php');
-?>
