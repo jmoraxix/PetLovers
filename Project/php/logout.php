0117ebf543d894d6b87abf138c8519ac0bd8eaf3
Elang89 <elang8899@gmail.com>
2015-04-18 10:46:26 -0600

    Por favor no tocar las cosas que ya estan listas

--- Project/php/logout.php
+++ Project/php/logout.php
@@ -4,10 +4,8 @@
   if(isset($_SESSION['name']) && isset($_SESSION['usertype'])){
 		unset($_SESSION['name']);
 		unset($_SESSION['usertype']);
-		//exit('1');
-  }
-  // else {
-		// exit ('0');
-  // }
-header('location: ../index.php');
+	    exit('1');
+  } else {
+		exit('0');
+	}
 ?>
