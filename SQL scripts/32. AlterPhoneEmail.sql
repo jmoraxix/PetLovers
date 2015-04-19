2af8bc978b1fb17b2982d6079eac3a8077c5fc51
Elang89 <elang8899@gmail.com>
2015-04-18 18:15:34 -0600

    cambio pequeño a primary keys de phone y email

--- SQL scripts/32. AlterPhoneEmail.sql
+++ SQL scripts/32. AlterPhoneEmail.sql
@@ -7,14 +7,14 @@ Script for adding constraints and keys to Phone and Email tables
 Adds constraints for primary keys
 Adds primary keys to index
 */
-ALTER TABLE Phone
-ADD CONSTRAINT pk_phone PRIMARY KEY (phone_number)
-USING INDEX
-TABLESPACE PetLovers_Indexes PCTFREE 20
-STORAGE (initial 10k next 10k pctincrease 0);
-
-ALTER TABLE Email
-ADD CONSTRAINT pk_email PRIMARY KEY (email)
-USING INDEX
-TABLESPACE PetLovers_Indexes PCTFREE 20
-STORAGE (initial 10k next 10k pctincrease 0);
+ALTER TABLE PHONE
+      ADD CONSTRAINT pk_phone PRIMARY KEY (person_id,phone_number)
+      USING INDEX
+      TABLESPACE PetLovers_Indexes PCTFREE 20
+      STORAGE (initial 10k next 10k pctincrease 0);
+      
+ALTER TABLE EMAIL 
+      ADD CONSTRAINT pk_email PRIMARY KEY (person_id,email)
+      USING INDEX
+      TABLESPACE PetLovers_Indexes PCTFREE 20
+      STORAGE (initial 10k next 10k pctincrease 0);
